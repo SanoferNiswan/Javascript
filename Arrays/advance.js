50ff30bb7f00ecad367f98c5146cf075d3c614f4
sanoferniswan <sanoferniswan786@gmail.com>
2024-11-05 17:03:36 +0530

    msg

--- Arrays/advance.js
+++ Arrays/advance.js
@@ -13,3 +13,13 @@ console.log(ar.flat(Infinity));
 const a6 = [1, 2, 3];
 const results = a6.flatMap(x => [x, x * 2]);
 console.log(results); // Output: [1, 2, 2, 4, 3, 6]
+
+
+a=[1,2,3,4,5,6];
+const odd=a.filter(x=>x%2!=0);
+const even=a.filter(x=>x%2==0);
+odd.sort((a,b)=>a-b);
+even.sort((a,b)=>b-a);
+const res=odd.concat(even);
+console.log();
+console.log(res);
